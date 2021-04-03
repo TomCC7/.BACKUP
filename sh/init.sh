c1cb2d76d9503a97b45d75e4c29d93cf2cb43ffd
cc <cc20070411@163.com>
2021-04-03 10:07:52 +0800

    [PATCH]fix bug: ownership of backup directories

--- sh/init.sh
+++ sh/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function json_append {
-	cat <<< $(jq "$1" "$2") > "$2"
+  cat <<< $(jq "$1" "$2") > "$2"
 }
 
 function sync_dir() {
@@ -19,9 +19,10 @@ function sync_dir() {
 
 # getting sudo privelege
 if [[ "$UID" -ne 0 ]]; then
-	echo "Asking for sudo privelege..."
-	exec sudo -E "$0" "$@"
-	exit 0
+  echo "Asking for sudo privelege..."
+  export USR=$USER
+  sudo -E "$0" "$@"
+  exit 0
 fi
 
 # defining vars
@@ -39,15 +40,15 @@ fi
 
 if ! [[ -f config.json ]] || [[ $(jq .user config.json) == 'null' ]];
 then
-	# creating config.json
-	sudo -u $USER touch config.json
-	# if empty, write the root object
-	if [[ $(cat config.json) == '' ]];
-	then
-		echo "{}" > config.json
-		json_append ".back_dir=\"$BACK_DIR\"" config.json
-	fi
-	json_append ".user=\"$USER\"" config.json
+  # creating config.json
+  sudo -u $USR touch config.json
+  # if empty, write the root object
+  if [[ $(cat config.json) == '' ]];
+  then
+    echo "{}" > config.json
+    json_append ".back_dir=\"$BACK_DIR\"" config.json
+  fi
+  json_append ".user=\"$USR\"" config.json
 fi
 
 # finding files...
@@ -71,3 +72,5 @@ do
   fi
   echo "$FILE linked"
 done
+chown -R $USR:$USR $DIR/backup/* # change ownership
+echo "All FILE LINKED"
